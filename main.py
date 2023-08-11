130a4238b3fcf47d935da21e5ce3d18172dae1c4
joshuaStefanLao <lao.joshuas@gmail.com>
2023-08-11 06:24:29 -0700

    Updated Trie class with contact list method that returns possible options

--- main.py
+++ main.py
@@ -25,3 +25,29 @@ class Trie:
                 node.children[char] = TrieNode()
             node = node.children[char]
         node.is_end_of_word = True
+    
+    def contacts_list(self, input):
+        contacts = []
+        node = self.root
+
+
+        for char in input:
+            if char not in node.children:
+                return contacts
+            else:
+                node = node.children[char]
+        
+        def dfs(input, node):
+            input_copy = input
+
+            if not node:
+                return
+            if node.is_end_of_word:
+                contacts.append(input_copy)
+     
+            for child in node.children:
+                dfs(input_copy + child, node.children[child])
+        
+        dfs(input, node)
+        return contacts
+
